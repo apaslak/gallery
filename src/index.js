108a1a873f038a2ac734059f3a7715cbeb302d6d
Amy Paslak <amy.paslak@vibes.com>
2019-05-04 18:53:08 -0500

    support ie11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
